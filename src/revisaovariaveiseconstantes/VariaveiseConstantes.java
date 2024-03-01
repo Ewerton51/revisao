d832c827a042271984fb908e53ce33444f508b8b
unknown <ewertonpersa1416@gmail.com>
2024-03-01 10:39:06 -0300

    Laço de Repetição WHILE

--- src/revisaovariaveiseconstantes/VariaveiseConstantes.java
+++ src/revisaovariaveiseconstantes/VariaveiseConstantes.java
@@ -4,31 +4,49 @@ import java.util.Scanner;
 
 public class VariaveiseConstantes {
 
-	public static void main(String[] args) {
-		
-		int opcao;
-		Scanner ler = new Scanner(System.in);
-		
-		System.out.println("# ## ### #### #########   Menu   ########## #### ### ## #");
-		System.out.println("\n--Digite 1 para ver a indicação de um livro--");
-		System.out.println("\n--Digite 2 para ler uma frase motivacional--");
-		System.out.println("\n--Digite 3 para receber uma indicação de música--");
-		opcao = ler.nextInt();
-		
-		switch(opcao) {
-			case 1:
-				System.out.println("Livro: As Crônicas de Nárnia - Volume único1");
-				break;
-			case 2:
-				System.out.println("Se você cair, levante! Não dá para andar deitado.");
-				break;
-			case 3:
-				System.out.println("Galantis - Gold Dust");
-				break;
-				default:
-					System.out.println("Opção inválida!");
-		}		
-		
-	}
+    public static void main(String[] args) {
+
+        String continua = "s";
+        int numero1, numero2, resultado;
+        Scanner ler = new Scanner(System.in);
+
+        while (!continua.equals("n")) {
+            System.out.println("Digite o primeiro valor: ");
+            numero1 = ler.nextInt();
+
+            System.out.println("Digite o segundo valor: ");
+            numero2 = ler.nextInt();
+
+            resultado = numero1 + numero2;
+
+            System.out.println("\nO resultado da soma é: " + resultado);
+
+            System.out.println("\n++++++++++++++++MENU++++++++++++++++++");
+            System.out.println("Deseja finalizar o exercício ou passar para o próximo?");
+            System.out.println("\nDigite S para o próximo OU digite N para finalizar o programa: ");
+            continua = ler.next();
+            System.out.println("++++++++++++++++++++++++++++++++++");
+            
+            if (!continua.equals("n")) {
+                System.out.println("\n++++++++++++++++Próximo exercício++++++++++++++++++");
+
+                int result, num, cont = 1;
+
+                while (cont < 4) {
+                    System.out.println("Digite o " + cont + "º número:");
+                    num = ler.nextInt();
+
+                    result = num * 3;
+                    System.out.println(num + " x 3 = " + result);
+                    System.out.println("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+                    cont++;
+                }
+                
+            }
+            System.out.println("\t\t_________Fim do programa_________");
+        }
+
+        
+    }
 
 }
